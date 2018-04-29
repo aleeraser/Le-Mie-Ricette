8429b77f4f82ebe03da01d38c00b8f22114537a5
aleeraser <alezini94@hotmail.it>
2018-04-29 21:14:46 +0200

    Copy&paste error in server.sh

--- server.sh
+++ server.sh
@@ -53,7 +53,7 @@ else
     # perform setup if flaskenv folder is not found
     [ -d "./flaskenv" ] || setup
 
-    if [ "$1" == "start" ]; then
+    if [ "$1" == "setup" ]; then
         setup
     elif [ "$1" == "start" ]; then
         venv_activate && gunicorn app:app -b 0.0.0.0:5001 -p "le_mie_ricette.pid" -D && echo "Server started"
